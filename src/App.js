289f04b15718de2a938fdb9ec0f4ffad72786122
Achmad Sobari <achmadsobari@Barrys-MacBook-Pro.local>
2022-06-28 15:25:17 +0700

    pengumpulan tugas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
-
+import Header from './components/Header';
+import Contact from './components/Contact';
+import contacts from './data/contacts.json';
 // Uncomment untuk memuat daftar kontak
 // import contacts from './data/contacts.json';
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <div className='container'>
+        <Header />
+        <main className='main'>
+          {contacts.map((contact, index) => (
+            <Contact data={contact} key={contact.phone} />
+          ))}
+        </main>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
 
-const Header = () => {
-    return (
-        <></>
-    )
-}
+const Header = (props) => {
+  return (
+    <header className='header'>
+      <h1 className='header__title'> Call a Friend</h1>
+      <p className='header__subtitle'> your friendly contact app </p>
+    </header>
+  );
+};
 
 export default Header;
